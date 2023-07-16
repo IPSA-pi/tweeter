23b10abd83987131993771e3a1b760d89feae8de
Ian Sebleius <sebeliusancira@gmail.com>
2023-07-15 17:52:43 -0700

    add validation for ajax post to check that user didn't provide an empty string or null and the charcount is within limit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,41 +5,42 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// import { format, render, cancel, register } from 'timeago.js';
-// const format = require('timeago');
-
 const createTweetElement = function (tweetObj) {
   // console.log(format(tweetObj.created_at));
-  const tweetTemplate = $('<article>');
-  tweetTemplate.prepend(`
-  <article class="tweet-cont">
-      <header class="tweet-header">
-        <span class="user-info">
-          <img src="https://i.imgur.com/73hZDYK.png" alt="Robert Bresson portrait">
-          <p>${tweetObj.user.name}</p>
-        </span>
-        <span class="handle">
-          <p>${tweetObj.user.handle}</p>
-        </span>
-      </header>  
-      <p class="tweet">${tweetObj.content.text}</p>  
-      <footer>
-        <p class="when">${timeago.format(tweetObj.created_at)}</p>
-        <span class="icons">
-          <span class="fa-solid fa-flag"></span>
-          <span class="fa-solid fa-retweet"></span>
-          <span class="fa-solid fa-heart"></span>
-        </span>
-      </footer>
-    </article>
-  `);
-  return tweetTemplate;
+  /// const tweetTemplate = $('<article>');
+  // tweetTemplate.append(`
+  const article = `<article class="tweet-cont">
+        <header class="tweet-header">
+          <span class="user-info">
+            <img src="https://i.imgur.com/73hZDYK.png" alt="Robert Bresson portrait">
+            <p>${tweetObj.user.name}</p>
+          </span>
+          <span class="handle">
+            <p>${tweetObj.user.handle}</p>
+          </span>
+        </header>  
+        <p class="tweet">${tweetObj.content.text}</p>  
+        <footer>
+          <p class="when">${timeago.format(tweetObj.created_at)}</p>
+          <span class="icons">
+            <span class="fa-solid fa-flag"></span>
+            <span class="fa-solid fa-retweet"></span>
+            <span class="fa-solid fa-heart"></span>
+          </span>
+        </footer>
+        <article>`;
+  return article;
+  // return tweetTemplate;
 };
 
 const renderTweets = function (tweetsArray) {
-  const tweetsHtml = [];
-  tweetsArray.forEach((t) => tweetsHtml.push(createTweetElement(t)));
-  return $('.tweet-cont').append(tweetsHtml);
+  // const tweetsHtml = [];
+  $('.tweet-container').empty();
+  tweetsArray.forEach((t) => {
+    // tweetsHtml.unshift(createTweetElement(t));
+    const result = createTweetElement(t);
+    $('.tweets-container').prepend(result);
+  });
 };
 
 const loadTweets = () => {
@@ -48,7 +49,6 @@ const loadTweets = () => {
     type: 'GET',
     dataType: 'json',
     success: (result) => {
-      console.log(result);
       renderTweets(result);
     },
     error: (error) => {
@@ -57,27 +57,35 @@ const loadTweets = () => {
   });
 };
 
-// loadTweets();
-
-const postTweet = () => {
-  const data = $('.tweetForm').serialize();
-  console.log(data);
-  $.ajax({
-    type: 'POST',
-    url: '/tweets',
-    data,
-  }).then((result) => {
-    console.log(result);
-  }).catch((error) => {
-    console.log(error);
-  });
-};
-
 $(document).ready(function () {
-  renderTweets(loadTweets());
-  $('.tweetBtn').on('submit', (event) => {
-    console.log(event);
+  loadTweets();
+  $('.tweetForm').submit((event) => {
     event.preventDefault();
-    postTweet();
+    const data = $('.tweetForm').serialize();
+
+    if (data === 'text=' || data === 'null') {
+      alert('please provide input');
+      return false;
+    }
+
+    if (data.length > 145) {
+      alert("you're tweet is to long");
+      return false;
+    }
+
+    console.log('data: ', data);
+    // 1. Place holder to make all the validations
+    // For e.g. validate for the null / empty or you want to check for 140 characters
+    $.ajax({
+      url: '/tweets',
+      type: 'POST',
+      data,
+      success: (result) => {
+        loadTweets();
+      },
+      error(err) {
+        console.log('There was an error', err);
+      },
+    });
   });
 });
